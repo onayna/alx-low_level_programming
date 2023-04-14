c4b8ba64f2455a7acc00ed838aa3c2f5fada5ea4
onayna <rkayaeomaima@gmail.com>
2023-04-14 16:43:52 -0700

    m

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,6 +11,7 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
+
 /*functions for 101-mul.c */
 
 /**

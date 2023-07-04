e3b5dbe1a40f92c9dbd7cc2da596a1ed464bcc7e
onayna <rkayaeomaima@gmail.com>
2023-07-04 15:51:28 -0700

    m

--- 0x13-more_singly_linked_lists/0-main.c
+++ 0x13-more_singly_linked_lists/0-main.c
@@ -25,7 +25,7 @@ int main(void)
     new->n = 9;
     new->next = head;
     head = new;
-    n = print_listint(head);
+    n = pop_listint(head);
     printf("-> %lu elements\n", n);
     free(new);
     return (0);

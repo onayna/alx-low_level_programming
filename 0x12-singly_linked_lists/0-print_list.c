a0ba24868c89be9de09ec749a787524b9ffaa140
onayna <rkayaeomaima@gmail.com>
2023-08-24 16:43:18 -0700

    m

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,6 +2,15 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
 size_t print_list(const list_t *h) {
     size_t count = 0;  // Initialize a counter for the number of nodes
 

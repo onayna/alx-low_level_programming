14e5f6ec023a1cb2305fe8a989fbfa35da74af1f
onayna <rkayaeomaima@gmail.com>
2023-08-25 11:49:13 -0700

    m

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,18 +12,18 @@
  */
 
 size_t print_list(const list_t *h) {
-    size_t count = 0;  // Initialize a counter for the number of nodes
+    size_t count = 0;  /*  Initialize a counter for the number of nodes */
 
     while (h != NULL) {
-        // Check if the string in the node is NULL
+        /*  Check if the string in the node is NULL */
         if (h->str == NULL) {
             printf("[0] (nil)\n");
         } else {
             printf("[%d] %s\n", h->len, h->str);
         }
 
-        count++;  // Increment the node count
-        h = h->next;  // Move to the next node
+        count++;  /*  Increment the node count */
+        h = h->next;  /*  Move to the next node */
     }
 
     return count;

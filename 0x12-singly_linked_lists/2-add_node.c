dd6f8360fe2671059bae7ae46f6573653dab805a
onayna <rkayaeomaima@gmail.com>
2023-08-25 16:42:11 -0700

    m

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,39 +1,34 @@
-
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * add_node - Adds a new node at the beginning of a list_t list.
- * @head: Pointer to a pointer to the head of the list.
- * @str: String to be duplicated and added to the new node.
+ * @head: Pointer to the head of the list.
+ * @str: String to be stored in the new node.
  *
- * Return: Address of the new element, or NULL if it failed.
+ * Return: Address of the new head.
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    if (str == NULL)
-        return (NULL);
+	list_t *new_node;
+	size_t str_len = strlen(str);
 
-    list_t *new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        perror("Memory allocation failed");
-        exit(EXIT_FAILURE);
-    }
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new->str = strdup(str);
-    if (new->str == NULL)
-    {
-        perror("String duplication failed");
-        free(new);
-        exit(EXIT_FAILURE);
-    }
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new->len = 0;
-    while (new->str[new->len] != '\0')
-        new->len++;
+	new_node->len = str_len;
+	new_node->next = *head;
+	*head = new_node;
 
-    new->next = *head;
-    *head = new;
-
-    return (new);
+	return (*head);
 }

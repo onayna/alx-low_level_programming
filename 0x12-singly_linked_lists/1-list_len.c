d8050fd3ead0faf785fed6b73e820d439b1bf8ef
onayna <rkayaeomaima@gmail.com>
2023-08-25 12:09:46 -0700

    m

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stddef.h>
+#include "lists.h" 
+
+/**
+ _ list_len - Returns the number of elements in a linked list.
+ _ @h: Pointer to the head of the linked list.
+ _
+ _ Return: the final value of count, which is the number of nodes in the linked list.
+ */
+size_t list_len(const list_t *h)
+{
+    size_t count = 0; /*count variable is initialized to keep track of the number of nodes*/
+    const list_t *current = h;
+
+    while (current != NULL)
+    {
+        count++;
+        current = current->next;
+    }
+
+    return count;
+}

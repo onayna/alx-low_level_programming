9db98ee8863ebc5f617576eb2180e0270ba59be0
onayna <rkayaeomaima@gmail.com>
2023-08-25 17:37:09 -0700

    m

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,9 +5,7 @@
  * */
  #include <stdio.h>
 
-void my_constructor(void) __attribute__((constructor));
-
-void my_constructor(void)
+void __attribute__((constructor)) my_constructor()
 {
     printf("You're beat! and yet, you must allow,\n");
     printf("I bore my house upon my back!\n");

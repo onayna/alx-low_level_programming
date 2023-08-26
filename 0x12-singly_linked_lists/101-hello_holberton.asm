1784a79cce9df7ea0ba83c576a1fa0639f1aed3c
onayna <rkayaeomaima@gmail.com>
2023-08-25 17:48:36 -0700

    m

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,28 @@
+section .data
+    hello db "Hello, Holberton,", 0
+    format db "%s", 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    push rdi               ; Preserve registers
+    push rsi
+    push rdx
+    push rcx
+    push r8
+    push r9
+    
+    mov rdi, format        ; Load the format string
+    lea rsi, [hello]       ; Load the address of the hello string
+    call printf           ; Call printf function
+    
+    pop r9                ; Restore registers
+    pop r8
+    pop rcx
+    pop rdx
+    pop rsi
+    pop rdi
+    
+    ret                    ; Return from main

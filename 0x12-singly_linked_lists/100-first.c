340379d24b7d079d470f197662a69ceb95ed28b6
onayna <rkayaeomaima@gmail.com>
2023-08-25 17:24:24 -0700

    m

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,13 +9,8 @@
 void before_main(void) {
     printf("You're beat! and yet, you must allow,\n");
     printf("I bore my house upon my back!\n");
-}
-int main(void)
-{
-    before_main(); 
-    return 0;
-}
 
+}
 
 
 

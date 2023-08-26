4e9bda7470d8f7f21312bfd52d4abc140554a75c
onayna <rkayaeomaima@gmail.com>
2023-08-25 17:57:47 -0700

    m

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -7,23 +7,15 @@ section .text
     extern printf
 
 main:
-    push rdi               ; Preserve registers
-    push rsi
-    push rdx
-    push rcx
-    push r8
-    push r9
+    push rbp                ; Preserve the base pointer
+    mov rdi, format         ; Load the format string
+    lea rsi, [hello]        ; Load the address of the hello string
+    call printf             ; Call printf function
+    pop rbp                 ; Restore the base pointer
+    ret                     ; Return from main
 
-    mov rdi, format        ; Load the format string
-    lea rsi, [hello]       ; Load the address of the hello string
-    call printf           ; Call printf function
 
-    pop r9                ; Restore registers
-    pop r8
-    pop rcx
-    pop rdx
-    pop rsi
-    pop rdi
 
-    ret                    ; Return from main
+
+
 

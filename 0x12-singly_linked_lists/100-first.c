4e1c6b21c5dced6e3a63a70a99cbfb7b6129bdac
onayna <rkayaeomaima@gmail.com>
2023-08-25 17:10:38 -0700

    m

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program.
+ * before_main - function executed before main
+ * Return: Always 0 (Success).
+ */
+
 void before_main(void) {
     printf("You're beat! and yet, you must allow,\n");
     printf("I bore my house upon my back!\n");
+}
+int main(void)
+{
+    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+    return (0);
 }

f4cb0a517d833cff1d5075d74a461062998ea378
onayna <rkayaeomaima@gmail.com>
2023-08-25 18:02:08 -0700

    m

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,18 @@
-section .data
-    hello db "Hello, Holberton,", 0
-    format db "%s", 0
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-section .text
-    global main
-    extern printf
-
+	SECTION .text
+	extern printf
+	global main
 main:
-    push rbp                ; Preserve the base pointer
-    mov rdi, format         ; Load the format string
-    lea rsi, [hello]        ; Load the address of the hello string
-    call printf             ; Call printf function
-    pop rbp                 ; Restore the base pointer
-    ret                     ; Return from main
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
 
 
 

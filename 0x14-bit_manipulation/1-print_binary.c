c00c005a9523dd09cb8c79c30223ca66abc1507f
onayna <rkayaeomaima@gmail.com>
2023-09-01 13:12:52 -0700

    m

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,8 +1,10 @@
+
 #include "main.h"
 
 /**
  * print_binary - Prints the binary representation of a number.
  * @n: The number to print in binary.
+ * * Return: no return.
  */
 void print_binary(unsigned long int n)
 {

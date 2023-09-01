70acf3fb7fa2d299c1659cff7158c6dccf00a180
onayna <rkayaeomaima@gmail.com>
2023-09-01 13:58:29 -0700

    m

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,10 +9,14 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-    if (!n || index >= (sizeof(unsigned long int) * 8))
-        return -1;  // Error
+ unsigned long int mask;
+    if (!n || index >= (sizeof(unsigned long int) * 8)) {
+        return -1; 
+    }
 
-    *n &= ~(1UL << index); // Clear the bit to 0
-
-    return 1; // Success
+    mask = 1UL << index;
+    
+    *n &= ~mask; 
+    
+    return 1; 
 }

362d08323be95186a0fcfdcf11e96f006ea13e65
onayna <rkayaeomaima@gmail.com>
2023-09-16 04:48:44 -0700

    m

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,46 +11,33 @@
  *
  * Return: The actual number of letters read and printed.
  *         0 if the file cannot be opened, read, or if other errors occur.
- */
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, rd, wd;
+	char *buff = malloc(sizeof(char) * letters);
 
-ssize_t read_textfile(const char *filename, size_t letters) {
-    int fd;            
-    ssize_t bytes_read, bytes_written;
-    char *buffer;
+	if (filename == NULL)
+	{
+		return (0);
+	}
 
-   
-    if (!filename)
-        return (0);
-
-   
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
-
-    
-    buffer = malloc(sizeof(char) * letters);
-    if (!buffer) {
-        close(fd);
-        return (0);
-    }
-
-    bytes_read = read(fd, buffer, letters);
-    if (bytes_read == -1) {
-        close(fd);
-        free(buffer);
-        return (0);
-    }
-
- 
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read) {
-        close(fd);
-        free(buffer);
-        return (0);
-    }
-
-   
-    close(fd);
-    free(buffer);
-    return (bytes_read);
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	rd = read(fd, buff, letters);
+	if (rd == -1)
+	{
+		return (0);
+	}
+	wd = write(STDOUT_FILENO, buff, rd);
+	if (wd == -1)
+	{
+		return (0);
+	}
+	close(fd);
+	free(buff);
+	return (wd);
 }

91f7a8f9236e677b516e66908d5bf01a4ecc4055
onayna <rkayaeomaima@gmail.com>
2023-09-17 08:00:09 -0700

    m

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,25 +11,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, len, write_status;
+ int fd, len, write_status;
 
-    if (filename == NULL)
+ if (filename == NULL){
         return (-1);
+ }
 
+ fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (fd == -1)
+    if (fd == -1) {
         return (-1);
+    }
 
-    if (text_content != NULL)
-    {
+    if (text_content != NULL) {
        
-        for (len = 0; text_content[len] != '\0'; len++)
-            ;
+    for (len = 0; text_content[len] != '\0'; len++); {
 
        
-        write_status = write(fd, text_content, len);
-        if (write_status == -1)
+        write_status = write(fd, text_content, len); }
+    if (write_status == -1)
         {
             close(fd);
             return (-1);

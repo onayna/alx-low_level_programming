ee4bb69341f34fb62c24c21ccdd5fdfc8006659c
onayna <rkayaeomaima@gmail.com>
2023-09-17 08:04:03 -0700

    m

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,42 +1,35 @@
 #include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
-
 /**
- * create_file - Creates a file and writes text content to it.
- * @filename: The name of the file to create.
- * @text_content: A NULL-terminated string to write to the file.
+ * create_file - create a file
+ * @filename: filename
+ * @text_content: text content of the file
  *
- * Return: 1 on success, -1 on failure.
+ * Description: return the required result
+ *
+ * Return: return integer  value
  */
 int create_file(const char *filename, char *text_content)
 {
- int fd, len, write_status;
+	int fd, lenght;
+	ssize_t res_write;
 
- if (filename == NULL){
-        return (-1);
- }
-
- fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-
-    if (fd == -1) {
-        return (-1);
-    }
-
-    if (text_content != NULL) {
-       
-    for (len = 0; text_content[len] != '\0'; len++); {
-
-       
-        write_status = write(fd, text_content, len); }
-    if (write_status == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
-
-  
-    close(fd);
-    return (1);
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		lenght = 0;
+		while (*(text_content + lenght) != '\0')
+			lenght++;
+		res_write = write(fd, text_content, lenght);
+		if (res_write == -1)
+		{
+			write(1, "fails", 6);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
 }
